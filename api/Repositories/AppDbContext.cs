3cf0df641a6c00a0008c13616afad09852f08563
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2024-01-02 17:51:57 -0300

    initial commit

---
+++ api/Repositories/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repositories;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options): base(options) { }
+
+    public DbSet<BlogPost> Posts { get; set; }
+}
