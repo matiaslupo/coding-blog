da7e9b2906288deed39c5c6a0edfe5362141c2b0
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2024-01-03 18:04:18 -0300

    Created domain model for Categories

---
+++ api/Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models.Domain;
+
+public class BlogPost
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    public string? ShortDescription { get; set; }
+    [Required]
+    public string Content { get; set; } = string.Empty;
+    public string? FeaturedImageUrl { get; set; }
+    public string? UrlHandle { get; set; }
+    public DateTime PublishedDate { get; set; }
+    [Required]
+    public string Author { get; set; } = string.Empty;
+    public bool IsVisible { get; set; } = true;
+}
---
+++ api/Web/Models/Domain/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web.Models.Domain;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string? UrlHandle { get; set; }
+
+}
