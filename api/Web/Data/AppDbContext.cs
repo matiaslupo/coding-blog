4f1efca780e62427f8dbee64571d54b5b5d4351d
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2024-01-03 18:02:52 -0300

    Added DbContext

---
+++ api/Web/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Models.Domain;
+
+namespace Web.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options): base(options){ }
+
+    public DbSet<BlogPost> BlogPosts { get; set; }
+    public DbSet<Category> Categories { get; set; }
+}
