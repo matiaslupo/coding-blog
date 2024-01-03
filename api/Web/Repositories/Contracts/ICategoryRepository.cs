75a0fcffdc2fd20211c3e86f064cca14b6007c9c
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2024-01-03 18:05:37 -0300

    Created Categories repository

---
+++ api/Web/Repositories/Contracts/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Web.Models.Domain;
+
+namespace Web.Repositories.Contracts;
+
+public interface ICategoryRepository
+{
+    Task<Category> CreateAsync(Category category);
+
+}
---
+++ api/Web/Repositories/Implementations/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Data;
+using Web.Models.Domain;
+using Web.Repositories.Contracts;
+
+namespace Web.Repositories.Implementations;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly AppDbContext _dbContext;
+
+    public CategoryRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Category> CreateAsync(Category category)
+    {
+        await _dbContext.Categories.AddAsync(category);
+        await _dbContext.SaveChangesAsync();
+        return category;
+    }
+}
