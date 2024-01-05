553bd1a135c2ad5076fa6ee71a5b726541294416
Matías Lupo <64920999+matiaslupo@users.noreply.github.com>
2024-01-04 23:21:18 -0300

    Added categories route

--- webapp/src/app/app-routing.module.ts
+++ webapp/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin/categories',
+    loadChildren: () => 
+      import('./modules/categories/categories.module').then((m) => m.CategoriesModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
